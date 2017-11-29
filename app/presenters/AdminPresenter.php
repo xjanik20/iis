af8b21effb12e7126bc8c419ee720c22f9adae2b
xjanik20 <luo96@seznam.cz>
2017-11-29 15:41:52 +0100

    Admin presenter and template start

---
+++ app/presenters/AdminPresenter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+class AdminPresenter extends Nette\Application\UI\Presenter
+{
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderStudents()
+    {
+        $this->template->post = $this->database->table('Student');
+    }
+
+}
