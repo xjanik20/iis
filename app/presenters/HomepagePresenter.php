845e587e0c663d837c854dc5fa134a5e1d65a3de
Michal Hrabovský <hrab.m@seznam.cz>
2017-11-29 15:37:04 +0100

    Authenticator and database setup

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,8 +3,29 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI;
 
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    protected function createComponentLoginForm()
+    {
+        $form = new UI\Form;
+        $form->addText('name', 'Name:');
+        $form->addPassword('password', 'Password:');
+        $form->addSubmit('login', 'Sign up');
+        $form->onSuccess[] = [$this, 'loginFormSucceeded'];
+        return $form;
+    }
+
+    // called after form is successfully submitted
+    public function loginFormSucceeded(UI\Form $form, $values)
+    {
+
+
+        // ...
+        $this->flashMessage('You have successfully signed up.');
+        $this->redirect('Homepage:');
+
+    }
 }
