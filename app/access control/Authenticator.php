446fd4268f0b7266b82171fc52c11def5bdd4d0c
Michal Hrabovský <hrab.m@seznam.cz>
2017-11-30 14:48:03 +0100

    Setup Routing

--- app/access control/Authenticator.php
+++ app/access control/Authenticator.php
@@ -7,33 +7,33 @@ class LoginAuthenticator implements NS\IAuthenticator
 {
     public $db;
 
-    function __construct(ND\Connection $db){
+    function __construct(ND\Context $db){
         $this->db = $db;
     }
 
     function authenticate(array $credentials)
     {
         list($username, $password) = $credentials;
-        $row = $this->database->table('Student')->where('login', $username)->fetch();
+        $row = $this->db->table('Student')->where('login', $username)->fetch();
         if (row){
-            if ($password == $row->$password){
-                return new NS\Identity($row->id,'Student', ['username' => $username]);
+            if ($password == $row->heslo){
+                return new NS\Identity($row->id_st,'Student', ['username' => $username]);
             }
         }
 
-        $row = $this->database->table('Ucitel')->where('login', $username)->fetch();
+        $row = $this->db->table('Ucitel')->where('login', $username)->fetch();
         if (row){
-            if ($password == $row->$password){
-                return new NS\Identity($row->id,'Ucitel', ['username' => $username]);
+            if ($password == $row->heslo){
+                return new NS\Identity($row->id_uc,'Ucitel', ['username' => $username]);
             }
         }
 
-        $row = $this->database->table('Admin')->where('login', $username)->fetch();
+        $row = $this->db->table('Admin')->where('login', $username)->fetch();
         if(!row){
             throw new NS\AuthenticationException('User not found.');
         }
-        if ($password == $row->$password) {
-            return new NS\Identity($row->id,'Admin', ['username' => $username]);
+        if ($password == $row->heslo) {
+            return new NS\Identity($row->id_ad,'Admin', ['username' => $username]);
         }
         throw new NS\AuthenticationException('Invalid password.');
     }
