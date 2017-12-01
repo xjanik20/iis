626edd5e99ec70e563d173bed9e9264b54f266a7
xjanik20 <luo96@seznam.cz>
2017-12-01 12:24:50 +0100

    js turned on, add view hide script, css

---
+++ www/js/script.js
@@ -0,0 +1,6 @@
+// view/hide add div
+    if (document.getElementById(id).style.display === 'none')
+        document.getElementById(id).style.display = 'block';
+    else
+        document.getElementById(id).style.display = 'none';
+}
