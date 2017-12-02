0d16dd5e0419f12e73fc1eda3f865aeb4c859de0
Michal Hrabovský <hrab.m@seznam.cz>
2017-12-02 14:03:09 +0100

    teacher presenter skeleton

---
+++ app/presenters/TeacherPresenter.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Greyhound
+ * Date: 2.12.2017
+ * Time: 13:49
+ */
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI;
+use Nette\Database as ND;
+
+class StudentPresenter extends Nette\Application\UI\Presenter
+{
+    /** @var Nette\Database\Context */
+    protected $database;
+
+    /** @var \App\Model\Factories\SearchFormFactory @inject */
+    public $searchFormFactory;
+
+    protected $formFilter = "";
+    protected $filterSet = false;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        parent::__construct();
+        $this->database = $database;
+    }
+
+    protected function createComponentSearchForm()
+    {
+        $form = $this->searchFormFactory->create();
+        $form->onSuccess[] = [$this, 'searchFormSucceeded'];
+        return $form;
+    }
+
+    public function searchFormSucceeded(UI\Form $form, $values)
+    {
+        if ($values['filter']) {
+            $this->formFilter = $values;
+            $this->filterSet = true;
+        }
+    }
+}
